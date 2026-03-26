c8052638b18fc278e68ac202d225f1457f60cf8a
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-26 22:15:38 +0530

    added cors agin

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import cors from 'cors';
 
 dotenv.config();
 
