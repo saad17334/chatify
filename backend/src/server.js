81a7689de3b07e6423982d302202c9e62f8f1e1e
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-26 21:49:48 +0530

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,19 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is LIVE 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
