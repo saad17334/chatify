c42e8d860179a55ac09f159c9b53837f02d7a574
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-28 00:51:55 +0530

    Implemented Arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import { signup, login, logout, updateprofile} from '../controllers/auth.controller.js';
 import {protectRoute} from "../middleware/auth.middleware.js"
+import {arcjetProtection} from "../middleware/arcjet.middleware.js"
 
 const router = express.Router();
 
+router.get("/test", arcjetProtection, (req,res) =>{
+    res.status(200).json({message:"test route"});
+})
+router.use(arcjetProtection);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateprofile);
 
-router.get("/check",protectRoute, (req,res) =>(res.status(200).json(req.user)));
+router.get("/check", protectRoute, (req,res) =>(res.status(200).json(req.user)));
 
 export default router;
