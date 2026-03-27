130d6e9bff31642f3ba833f291aa864166ae2f83
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-27 20:09:03 +0530

    Implemented login and lgout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
-import {ENV} from "../lib/env.js"
-import {sendWelcomeEmail} from "../emails/emailHandlers.js"
+import { ENV } from "../lib/env.js"
+import { sendWelcomeEmail } from "../emails/emailHandlers.js"
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -40,13 +40,11 @@ export const signup = async (req, res) => {
             generateToken(savedUser._id, res);
 
 
-            try
-            {
-               await sendWelcomeEmail(savedUser.email ,savedUser.fullName, ENV.CLIENT_URL);
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
             }
-            catch(error)
-            {
-                console.error("Failed to send welcome email.",error);
+            catch (error) {
+                console.error("Failed to send welcome email.", error);
             }
 
             return res.status(201).json({
@@ -67,4 +65,41 @@ export const signup = async (req, res) => {
         console.log("Signup error:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+    } catch (error) {
+        console.error("Error in the login controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Loggout succefully" });
 };
