753862c6347ac7d00aff3582f34a375386b3c484
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-28 20:45:43 +0530

    Added SignUp Functionality

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -2,17 +2,22 @@ import { resendClient, sender } from "../lib/resend.js";
 import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 
 export const sendWelcomeEmail = async (email, name, clientURL) => {
-  const { data, error } = await resendClient.emails.send({
-    from: `${sender.name} <${sender.email}>`,
-    to: email,
-    subject: "Welcome to Chatify!",
-    html: createWelcomeEmailTemplate(name, clientURL),
-  });
+  try {
+    const { data, error } = await resendClient.emails.send({
+      from: `${sender.name} <${sender.email}>`,
+      to: "saadakram529@gmail.com", // dev workaround
+      subject: "Welcome to Chatify!",
+      html: createWelcomeEmailTemplate(name, clientURL),
+    });
 
-  if (error) {
-    console.error("Error sending welcome email:", error);
-    throw new Error("Failed to send welcome email");
+    if (error) {
+      console.error("Email error:", error);
+      return;
+    }
+
+    console.log("Welcome Email sent", data);
+
+  } catch (err) {
+    console.error("Email crash:", err.message);
   }
-
-  console.log("Welcome Email sent successfully", data);
 };
