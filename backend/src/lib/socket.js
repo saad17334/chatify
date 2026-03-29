85f2a08d66ce4c1c9e6535273ab6c3c79be8f184
Saad Akram <saad@Saads-MacBook-Air.local>
2026-03-29 17:47:27 +0530

    Bug related to socket middleware

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import { ENV } from "./env.js";
-import { socketAuthMiddleware } from "../middleware/socket.auth.Middleware.js";
+import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
 
 const app = express();
 const server = http.createServer(app);
